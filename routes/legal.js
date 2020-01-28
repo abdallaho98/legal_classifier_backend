0bb260ccda35f2acecca6c93eb4541adeefdfa97
Abdallah Herzallah <fa_herzallah@esi.dz>
2020-01-28 04:06:19 +0100

    User Router

--- routes/legal.js
+++ routes/legal.js
@@ -5,13 +5,14 @@ const Legal = require('../Models/Legal')
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.warn("Herre" , req.query)
+    res.status(200).send({success : true})
 });
 
 
 /* GET home page. */
 router.post('/add', function(req, res, next) {
     console.warn("process create")
-    Legal.create(req.body).then(data => {   res.send(data);}).catch(err => {res.status(400).send(err);})
+    Legal.create(req.body).then(data => {   res.send({success : true},data);}).catch(err => {res.status(400).send(err);})
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,15 @@ router.post('/register', function(req, res, next) {
 router.post('/login', function(req, res, next) {
   User.findOne(
       { email: req.body.email }
-  ).then(data => {   res.send(data);}).catch(err => {res.status(400).send(err);})
+  ).then(data => {
+    if (data != null)
+      res.send({success : true , data});
+    else res.send({success : false });
+  }).catch(err => {res.status(400).send({success : false });})
+});
+
+router.post('/', function(req, res, next) {
+  User.find({}).then(data => {res.send({success : true , data});}).catch(err => {res.status(400).send({success : false });})
 });
 
 module.exports = router;
