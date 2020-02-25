78471690b4921ae1f2b0af95b68fde35f17b8ecf
Abdallah Herzallah <fa_herzallah@esi.dz>
2020-02-25 04:18:13 +0100

    lilmit json

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const port = process.env.PORT || 3000
 Mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true }).then(res => {console.warn('connected')}).catch(err => {console.warn(err)});
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb' ,extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
